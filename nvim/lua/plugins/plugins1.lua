aec89b9c77f0ff333a96cd33cfa1bbdd8ffdadae
bai <bai@baturax.com>
2025-05-27 16:38:16 +0300

    ..

--- nvim/lua/plugins/plugins1.lua
+++ nvim/lua/plugins/plugins1.lua
@@ -1,9 +1,16 @@
 return {
 
   {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true
+    'altermo/ultimate-autopair.nvim',
+    event={'InsertEnter','CmdlineEnter'},
+    branch='v0.6', --recommended as each new version will have breaking changes
+    opts={
+        --Config goes here
+    },
+  },
+
+  {
+    "windwp/nvim-ts-autotag"
   },
 
   {
