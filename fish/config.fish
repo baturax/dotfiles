d6da1a2441bec5f6e1ef3344d0ca5f6168e3d66f
Baturax <bai@baturax.com>
2025-09-17 23:02:02 +0300

    fish

---
+++ fish/config.fish
@@ -0,0 +1,27 @@
+# bai
+
+alias gclone="git clone"
+alias gclone1="git clone --depth 1"
+alias gcloner="git clone --recursive"
+alias gpush="git push"
+alias gcommit="git commit"
+
+alias c="clear"
+alias lg="lazygit"
+
+alias pi="pacman -S"
+alias pr="pacman -R"
+alias prc="pacman -Rcs"
+alias prd="pacman -Rdd"
+
+function l
+	command ls --color=always $argv
+end
+
+function ls
+	command ls --color=always -A $argv
+end
+
+function fish_greeting
+    gofetch
+end
